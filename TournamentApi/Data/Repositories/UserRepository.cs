cbbecf3caaf364c2edfb112e45c6aae9838758f2
Tijs Martens <tijs.martens@student.hogent.be>
2019-03-08 22:22:52 +0100

    added (i)UserRepository

---
+++ TournamentApi/Data/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TournamentApi.Models;
+
+namespace TournamentApi.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly TournamentContext _context;
+        private readonly DbSet<User> _users;
+
+
+        public UserRepository(TournamentContext context)
+        {
+            _context = context;
+            _users = context.Users_domain;
+        }
+
+        public void Add(User user)
+        {
+            _users.Add(user);
+        }
+
+        public void Delete(User user)
+        {
+            _users.Remove(user);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _users.Include(u => u.Tournament)
+                 .OrderBy(u => u.TennisVlaanderenRanking)
+                 .ThenBy(u => u.FirstName)
+                 .ToList(); 
+
+        }
+
+        public User GetById(int id)
+        {
+            return _users
+                .Include(u => u.Tournament)
+                .SingleOrDefault(u => u.UserId == id);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ TournamentApi/Models/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TournamentApi.Models
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        void Add(User user);
+        void Delete(User user);
+        void SaveChanges();
+      
+    }
+}
